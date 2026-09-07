bd9b511600ce6daa649649fbe4dc6d042d685848
Jayden Russell <hermes-agent@users.noreply.github.com>
2026-09-07 02:06:16 -0400

    Bump to v1.8.5

--- mod_cbprofileslim.php
+++ mod_cbprofileslim.php
@@ -7,7 +7,7 @@
  * dependency on the sccard/cblogin module being on the page.
  * Top-level try/catch prevents any error from becoming a 500.
  *
- * @version 1.8.4
+ * @version 1.8.5
  */
 defined('_JEXEC') or die;
 
